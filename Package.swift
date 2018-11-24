4a0bb9343f58c1241b1cd3e6f3560e4fae4bf48b
Ron Pinz <ron.pinz@gmail.com>
2018-11-23 19:44:18 -0500

    add manifest for swift 5 update 4.2 manifest update CCairo submodule

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Cairo",
     dependencies: [
         .Package(url: "https://github.com/rpinz/CCairo", majorVersion: 1),
-        .Package(url: "https://github.com/rpinz/SwiftGObject", majorVersion: 2),
+        .Package(url: "https://github.com/rpinz/SwiftGObject", majorVersion: 2)
     ],
     swiftLanguageVersions: [3, 4]
 )
